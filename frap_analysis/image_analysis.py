ef66d9f6ca4eddbb1123732b9a9b0efb88571f49
acorbat <acorbat@df.uba.ar>
2018-10-30 15:45:07 -0300

    Added Image analysis for citoplasm. Seems to be working and leaves dataframe ready for fitting.

--- frap_analysis/image_analysis.py
+++ frap_analysis/image_analysis.py
@@ -207,3 +207,105 @@ def find_bleching_end_time(img, img_ble):
 
     return dead_time.total_seconds()
 
+
+# Citoplasm analysis
+
+def get_intensity_df(stack, bbox):
+    sel_stack = stack[:, bbox[0]:bbox[1], bbox[2]:bbox[3]]
+    area = (bbox[1] - bbox[0]) * (bbox[3] - bbox[2])
+    variable_dict = {'sum_intensity': np.nansum(sel_stack, axis=(1, 2)),
+                     'mean_intensity': np.nanmean(sel_stack, axis=(1, 2)),
+                     'std_intensity': np.nanstd(sel_stack, axis=(1, 2)),
+                     'median_intensity': np.nanmedian(sel_stack, axis=(1, 2)),
+                     'frame': [i for i in range(sel_stack.shape[0])],
+                     'area': [area, ] * sel_stack.shape[0]}
+
+    return pd.DataFrame(variable_dict)
+
+
+def load_and_correct_citoplasmic(path):
+    img = fv.FV1000(str(path))
+    path = img.get_last_path()
+    img = fv.FV1000(str(path))
+    img_ble = fv.FV1000(str(img.get_other_path(kind='ble')))
+
+    scales = img.get_scales()
+    time_step = scales['T']
+    stack = img.transpose_axes('CTYX')[0]
+
+    corrector = corr.Corrector()
+
+    bleached_bbox = img_ble.get_clip_bbox()
+    bleached_bbox = expand_bbox(bleached_bbox, pixs=10)
+
+    bleaching_stack = stack.copy()
+    bleaching_stack[:, bleached_bbox[0]:bleached_bbox[1], bleached_bbox[2]:bleached_bbox[3]] = np.nan
+    corrector.find_bleaching(bleaching_stack, time_step)
+
+    stack = corrector.subtract_and_normalize(stack, time_step)
+
+    bbox = img_ble.get_clip_bbox()
+    df = get_intensity_df(stack, bbox)
+    df['time'] = df.frame.apply(lambda x: x * time_step)
+
+    dead_time = find_bleching_end_time(img, img_ble)
+    df['time'] = df.time.apply(lambda x: x + dead_time)
+
+    return df
+
+
+def citoplasm_to_curves(df_pos_path, columns=['date', 'condition', 'experiment', 'cell'],
+                     squash_columns=['time', 'sum_intensity', 'mean_intensity',
+                                     'std_intensity', 'median_intensity', 'frame', 'area'],
+                     df_pre_path=None):
+
+    if df_pre_path is None:
+        parts = df_pos_path.stem.split('_')
+        parts[-1] = 'pre'
+        df_pre_path = df_pos_path.with_name('_'.join(parts) + '.pandas')
+
+    df_pos = pd.read_pickle(str(df_pos_path))
+    df_pre = pd.read_pickle(str(df_pre_path))
+
+    df = df_pos.append(df_pre, ignore_index=True)
+    df['cell'] = df.cell.apply(lambda x: '_'.join(x.split('_')[:2]))
+    columns.append('kind')
+
+    # Squash DataFrame of bleached particles only
+    new_df = pd.DataFrame()
+    for column_values, this_df in df.groupby(columns):
+
+        dict_to_df = {col: [this_df[col].values] for col in squash_columns}
+        this_df = pd.DataFrame(dict_to_df)
+        this_df['particle'] = 1
+        for val, col in zip(column_values, columns):
+            this_df[col] = val
+
+        new_df = new_df.append(this_df, ignore_index=True)
+
+    # Find initial intensity and normalize it
+    new_df['mean_pre_intensity'] = np.nan
+    new_df['std_pre_intensity'] = np.nan
+    new_df['intensity'] = np.nan
+    new_df['intensity'] = new_df['intensity'].astype(object)
+
+    for column_values, this_df in new_df.groupby(columns[:-1]):
+        this_pre_df = this_df.query('kind == "pre"')
+        this_pos_df = this_df.query('kind == "pos"')
+
+        mean_pre_intensity = np.mean(this_pre_df.mean_intensity.values[0])
+        std_pre_intensity = np.std(this_pre_df.mean_intensity.values[0])
+
+        try:
+            new_df.at[this_pre_df.index[0], 'mean_pre_intensity'] = mean_pre_intensity
+            new_df.at[this_pos_df.index[0], 'mean_pre_intensity'] = mean_pre_intensity
+            new_df.at[this_pre_df.index[0], 'std_pre_intensity'] = std_pre_intensity
+            new_df.at[this_pos_df.index[0], 'std_pre_intensity'] = std_pre_intensity
+            new_df.at[this_pre_df.index[0], 'intensity'] = np.asarray(
+                new_df.at[this_pre_df.index[0], 'mean_intensity'] / mean_pre_intensity)
+            new_df.at[this_pos_df.index[0], 'intensity'] = np.asarray(
+                new_df.at[this_pos_df.index[0], 'mean_intensity'] / mean_pre_intensity)
+        except IndexError:
+            print('date: %s, condition: %s, exp: %s, cell: %s is missing a period.' % column_values)
+
+    return new_df
